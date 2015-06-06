0a6aa0a95a1bdb0b1d5518ea9947ce48f7e15b3d
sebug <sebastian.gfeller@gmail.com>
2015-06-06 09:35:54 +0200

    First function

--- swift_decisions.swift
+++ swift_decisions.swift
@@ -1,1 +1,15 @@
+func getDateArgument() -> String? {
+    if Process.arguments.count >= 2 {
+        return Process.arguments[1]
+    } else {
+        return nil
+    }    
+}
+
 println("Usage: swift_decisions [date]")
+let dateArgument : String? = getDateArgument()
+println("The date argument is \(dateArgument)")
+
+
+
+
