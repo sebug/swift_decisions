fcc4a2362fccf5f90b42b47e815c1bdf615568ab
sebug <sebastian.gfeller@gmail.com>
2015-06-06 09:58:17 +0200

    Deciding on date

--- swift_decisions.swift
+++ swift_decisions.swift
@@ -1,17 +1,27 @@
 import Foundation
 
-func getDateArgument() -> String? {
+func getDateArgument() -> NSDate? {
     if Process.arguments.count >= 2 {
         let dateFormatter = NSDateFormatter()
-        return Process.arguments[1]
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.timeZone = NSTimeZone(forSecondsFromGMT: 0)
+        return dateFormatter.dateFromString(Process.arguments[1])
     } else {
         return nil
-    }    
+    }
+}
+
+func dateOrNow(dateOpt: NSDate?) -> NSDate {
+    if let date = dateOpt {
+        return date
+    } else {
+        return NSDate()
+    }
 }
 
 println("Usage: swift_decisions [date]")
-let dateArgument : String? = getDateArgument()
-println("The date argument is \(dateArgument)")
+let theDate = dateOrNow(getDateArgument())
+println("The date argument is \(theDate)")
 
 
 
