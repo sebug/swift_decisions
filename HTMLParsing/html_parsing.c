27bce5d28620b38d46abb8f373d66c21c8ce823c
sebug <sebastian.gfeller@gmail.com>
2015-06-06 18:29:01 +0200

    Includes Gumbo in the build now

--- HTMLParsing/html_parsing.c
+++ HTMLParsing/html_parsing.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <gumbo.h>
+
+void parseHTML() {
+  GumboOutput* output = gumbo_parse("<h1>Hello, World!</h1>");
+  gumbo_destroy_output(&kGumboDefaultOptions, output);
+}
 
 void getInput(int* output) {
   scanf("%i", output);
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 platform=$(xcrun --sdk macosx --show-sdk-path)
-clang -c HTMLParsing/html_parsing.c
+clang `pkg-config --cflags gumbo` -c HTMLParsing/html_parsing.c
 ar rcs libHTMLParsing.a html_parsing.o
 mv libHTMLParsing.a HTMLParsing/libHTMLParsing.a
-swiftc -sdk $platform -I HTMLParsing -L HTMLParsing -lHTMLParsing swift_decisions.swift
+swiftc -sdk $platform -I HTMLParsing -L HTMLParsing `pkg-config --libs gumbo` -lHTMLParsing swift_decisions.swift
 
